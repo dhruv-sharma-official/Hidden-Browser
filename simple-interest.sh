232ada8737a8073689e81a5a3aa7e8951c225fe4
Dhruv Sharma <tritans472@gmail.com>
2024-04-02 11:14:22 +0530

    added simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# simple-interest.sh file with description
+
+# Show a temporary decoy message 
+echo "first extract hiddenbrowser.rar file using winrar or tar extractor"
+echo "then run the project using readme file"
+
